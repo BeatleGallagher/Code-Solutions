2d7060182925f66ef81725863cdb8a74ffd8487c
Karel Jordaan <kareljordaan@yahoo.com>
2019-04-14 18:45:40 +0200

    Update problem_solutions_in_php.php

--- problem_solutions_in_php.php
+++ problem_solutions_in_php.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
-
-
 <html>
 	<head>
 	<title>PHP problem translations from javascript</title>
-
-	</head>
-		<body>
+</head>
+<body>
 <?php 
-	/* 
-	Have the function CheckNums(num1,num2) take both parameters being passed 
-	and return the string true if num2 is greater than num1, 
-	otherwise return the string false. If the parameter values are equal to each other 
-	then return the string -1. 
-	*/
+/* PHP SOLUTIONS
+Have the function CheckNums(num1,num2) take both parameters being passed 
+and return the string true if num2 is greater than num1, 
+otherwise return the string false. If the parameter values are equal to each other 
+then return the string -1. 
+*/
 function check_nums($num_one,$num_two){
 	if($num_two > $num_one){
 		echo "true";
@@ -67,10 +64,9 @@ function AlphabetSoup($str){
 AlphabetSoup("coderbyte");
 ?>
 			
-	</body>
-	<footer>
-		
-	</footer>
+</body>
+<footer>	
+</footer>
 </html>
 <script>
 	
