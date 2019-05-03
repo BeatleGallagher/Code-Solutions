aba7753319c1b006d9697b830b306c6c100c32b2
Karel Jordaan <kareljordaan@yahoo.com>
2019-05-03 10:30:00 +0200

    Update friends.php

--- friends.php
+++ friends.php
@@ -12,27 +12,29 @@ Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 Note: keep the original order of the names in the output.
 */
 function friend($friends){
-		$four_letter_friends = [];
+	$four_letter_friends = [];
 
-		for($index = 0;$index < count($friends);$index++){
-			if((is_numeric($friends[$index]) === FALSE)  &&  (strlen($friends[$index]) === 4)){
-				array_push($four_letter_friends,$friends[$index]);
-			}	
-		}
-		return print_r($four_letter_friends);
+	for($index = 0;$index < count($friends);$index++){
+		if((is_numeric($friends[$index]) === FALSE)  &&  (strlen($friends[$index]) === 4)){
+			array_push($four_letter_friends,$friends[$index]);
+		}	
+	}
+	return $four_letter_friends;
 }
 friend(["Ryan", "Kieran", "1234",1234,"Mark"]);
 ?>
+	
 <script>
-    // SOLUTION IN JAVASCRIPT
+// SOLUTION IN JAVASCRIPT
 function friend(friends){
     var fourLetterFriends = [];
+	
     for(var i = 0;i < friends.length;i++){
       if(isNaN(friends[i]) && friends[i].length === 4){
         fourLetterFriends.push(friends[i]);  
       } 
-	}
-	console.log(fourLetterFriends);
+    }
+    console.log(fourLetterFriends);
 }
 friend(["Ryan", "Kieran", "1234","Mark"]);
 
